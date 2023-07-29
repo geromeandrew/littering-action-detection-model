bc8a42e049e46d9146714342eadfac473335ace3
Gerome <geromeandrew7@gmail.com>
2023-07-29 15:05:32 +0800

    refactor code

--- actModels.py
+++ actModels.py
@@ -2,77 +2,45 @@ from keras import layers
 from keras.models import Sequential
 
 
-def convlstm_model(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
-    '''
-    This function will construct the required convlstm model.
-    Returns:
-        model: It is the required constructed convlstm model.
-    '''
-
-    # We will use a Sequential model for model construction
+def model_1(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
     model = Sequential()
 
-    # Define the Model Architecture.
-    ########################################################################################################################
-
-    model.add(layers.ConvLSTM2D(filters=4, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
-                                recurrent_dropout=0.2, return_sequences=True, input_shape=(SEQUENCE_LENGTH,
-                                                                                           IMAGE_SIZE, IMAGE_SIZE, 3)))
-
-    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),
-              padding='same', data_format='channels_last'))
+    model.add(layers.ConvLSTM2D(filters=4, kernel_size=(3, 3), activation='tanh', data_format="channels_last", 
+                                recurrent_dropout=0.2, return_sequences=True, 
+                                input_shape=(SEQUENCE_LENGTH, IMAGE_SIZE, IMAGE_SIZE, 3)))
+    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
     model.add(layers.TimeDistributed(layers.Dropout(0.2)))
 
     model.add(layers.ConvLSTM2D(filters=8, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
                                 recurrent_dropout=0.2, return_sequences=True))
-
-    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),
-              padding='same', data_format='channels_last'))
+    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
     model.add(layers.TimeDistributed(layers.Dropout(0.2)))
 
     model.add(layers.ConvLSTM2D(filters=14, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
                                 recurrent_dropout=0.2, return_sequences=True))
-
-    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),
-              padding='same', data_format='channels_last'))
+    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
     model.add(layers.TimeDistributed(layers.Dropout(0.2)))
 
     model.add(layers.ConvLSTM2D(filters=16, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
                                 recurrent_dropout=0.2, return_sequences=True))
-
-    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),
-              padding='same', data_format='channels_last'))
-    # model.add(TimeDistributed(Dropout(0.2)))
+    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),  padding='same', data_format='channels_last'))
+    model.add(layers.TimeDistributed(layers.Dropout(0.2)))
 
     model.add(layers.Flatten())
 
     model.add(layers.Dense(len(CLASSES_LIST), activation="softmax"))
 
-    ########################################################################################################################
-
-    # Display the models summary.
     model.summary()
-
-    # Return the constructed convlstm model.
     return model
 
 
-def LRCN_model(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
-    '''
-    This function will construct the required LRCN model.
-    Returns:
-        model: It is the required constructed LRCN model.
-    '''
 
-    # We will use a Sequential model for model construction.
+
+def model_2(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
     model = Sequential()
-
-    # Define the Model Architecture.
-    ########################################################################################################################
 
     model.add(layers.TimeDistributed(layers.Conv2D(16, (3, 3), padding='same', activation='relu'),
                                      input_shape=(SEQUENCE_LENGTH, IMAGE_SIZE, IMAGE_SIZE, 3)))
-
     model.add(layers.TimeDistributed(layers.MaxPooling2D((4, 4))))
     model.add(layers.TimeDistributed(layers.Dropout(0.25)))
 
@@ -81,26 +49,32 @@ def LRCN_model(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
     model.add(layers.TimeDistributed(layers.MaxPooling2D((4, 4))))
     model.add(layers.TimeDistributed(layers.Dropout(0.25)))
 
-    # model.add(layers.TimeDistributed(layers.Conv2D(
-    #     64, (3, 3), padding='same', activation='relu')))
-    # model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
-    # model.add(layers.TimeDistributed(layers.Dropout(0.25)))
-
-    # model.add(layers.TimeDistributed(layers.Conv2D(
-    #     64, (3, 3), padding='same', activation='relu')))
-    # model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
-    # # model.add(TimeDistributed(Dropout(0.25)))
-
     model.add(layers.TimeDistributed(layers.Flatten()))
-
     model.add(layers.LSTM(32))
-
     model.add(layers.Dense(len(CLASSES_LIST), activation='softmax'))
 
-    ########################################################################################################################
-
-    # Display the models summary.
     model.summary()
+    return model
 
-    # Return the constructed LRCN model.
+
+
+
+def model_3(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST):
+    model = Sequential()
+
+    model.add(layers.TimeDistributed(layers.Conv2D(16, (3, 3), padding='same', activation='relu'),
+                                     input_shape=(SEQUENCE_LENGTH, IMAGE_SIZE, IMAGE_SIZE, 3)))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((4, 4))))
+    model.add(layers.TimeDistributed(layers.Dropout(0.25)))
+
+    model.add(layers.TimeDistributed(layers.Conv2D(
+        32, (3, 3), padding='same', activation='relu')))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((4, 4))))
+    model.add(layers.TimeDistributed(layers.Dropout(0.25)))
+
+    model.add(layers.TimeDistributed(layers.Flatten()))
+    model.add(layers.Bidirectional(layers.LSTM(32)))
+    model.add(layers.Dense(len(CLASSES_LIST), activation='softmax'))
+
+    model.summary()
     return model
--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from utils.hubconf import custom
 from keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import TensorBoard
 from my_utils import VideoFrameGenerator
-from actModels import convlstm_model, LRCN_model
+from actModels import model_1, model_2, model_3
 
 
 seed_constant = 27
@@ -23,15 +23,13 @@ tf.random.set_seed(seed_constant)
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", type=str, required=True,
                 help="path to csv Data")
-# Specify the number of frames of a video that will be fed to the model as one sequence.
 ap.add_argument("-l", "--seq_len", type=int, default=20,
                 help="length of Sequence")
-
 ap.add_argument("-s", "--size", type=int, default=64,
                 help="size of video frame will be resized in our dataset")
-ap.add_argument("-m", "--model", type=str,  default='LRCN',
-                choices=['convLSTM', 'LRCN'],
-                help="select model type convLSTM or LRCN")
+ap.add_argument("-m", "--model", type=str,  default='model_1',
+                choices=['model_1', 'model_2', 'model_3'],
+                help="select model type")
 ap.add_argument("-e", "--epochs", type=int, default=70,
                 help="number of epochs")
 ap.add_argument("-b", "--batch_size", type=int, default=4,
@@ -54,30 +52,14 @@ yolov7_model_path = args["yolov7_model"]
 yolov7_conf = args["yolov7_conf"]
 gpu_status = args['gpu']
 
-# some global params
+# global params
 SIZE = (IMAGE_SIZE, IMAGE_SIZE)
 CHANNELS = 3
-
-# pattern to get videos and classes
 glob_pattern= DATASET_DIR + '/{classname}/*'
-
-# YOLOv7 Model
 yolov7_custom_model = custom(path_or_model=yolov7_model_path, gpu=gpu_status)
-
-# Data Extraction Start
 s_time = time.time()
-
-# Specify the list containing the names of the classes used for training. Feel free to choose any set of classes.
 CLASSES_LIST = sorted(os.listdir(DATASET_DIR))
 
-# for data augmentation
-# preprocessor = ImageDataGenerator(
-#     rotation_range=10,
-#     width_shift_range=0.1,
-#     height_shift_range=0.1
-# )
-
-# Create video frame generator
 train_gen = VideoFrameGenerator(
     classes=CLASSES_LIST, 
     glob_pattern=glob_pattern,
@@ -89,31 +71,30 @@ train_gen = VideoFrameGenerator(
     nb_channel=CHANNELS,
     yolov7_model=yolov7_custom_model,
     yolov7_conf=yolov7_conf,
-    # transformation=preprocessor,
     use_frame_cache=False
 )
 
-# Validation Generator
 valid_gen = train_gen.get_validation_generator()
 
-# Data Size
 train_size = int(train_gen.files_count)
 val_size = int(valid_gen.files_count)
 total_data = train_size + val_size
 
-# Model Selection
-if model_type == 'convLSTM':
-    print("[INFO] Selected convLSTM Model")
-    model = convlstm_model(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST)
-    print("[INFO] convLSTM Created Successfully!")
-elif model_type == 'LRCN':
-    print("[INFO] Selected LRCN Model")
-    model = LRCN_model(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST)
-    print("[INFO] LRCN Created Successfully!")
+if model_type == 'model_1':
+    print("[INFO] Selected Model 1")
+    model = model_1(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST)
+    print("[INFO] Model 1 Created Successfully!")
+elif model_type == 'model_2':
+    print("[INFO] Selected Model 2")
+    model = model_2(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST)
+    print("[INFO] Model 2 Created Successfully!")
+elif model_type == 'model_3':
+    print("[INFO] Selected Model 3")
+    model = model_3(SEQUENCE_LENGTH, IMAGE_SIZE, CLASSES_LIST)
+    print("[INFO] Model 3 Created Successfully!")
 else:
     print('[INFO] Model NOT Choosen!!')
 
-# Model Dir
 path_to_model_dir = f'{model_type}'
 if not os.path.isdir(path_to_model_dir):
     os.makedirs(path_to_model_dir, exist_ok=True)
@@ -126,31 +107,25 @@ else:
 
 png_name = f'{model_type}_model_str.png'
 path_to_model_str = os.path.join(path_to_model_dir, png_name)
-# Plot the structure of the contructed model.
 tf.keras.utils.plot_model(model, to_file=path_to_model_str,
            show_shapes=True, show_layer_names=True)
 print(f'[INFO] Successfully Created {png_name}')
 
-# Create an Instance of Early Stopping Callback
 early_stopping_callback = EarlyStopping(
     monitor='val_loss', patience=15, mode='min', restore_best_weights=True)
+tensorboard_callback = TensorBoard(log_dir=f'{model_type}_logs', histogram_freq=1)
 
-tensorboard_callback = TensorBoard(log_dir='logs', histogram_freq=1)
-
-# Compile the model and specify loss function, optimizer and metrics values to the model
 precision = tf.keras.metrics.Precision()
 recall = tf.keras.metrics.Recall()
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='Adam', metrics=['accuracy', precision, recall, tf.keras.metrics.AUC()])
+              optimizer='Adam', metrics=['accuracy', precision, recall])
 
 print(f'[INFO] {model_type} Model Training Started...')
 
-# MLFlow
 mlflow.set_experiment('Action Recognition')
 with mlflow.start_run(run_name=f'{model_type}_model'):
     mlflow.tensorflow.autolog()
-    # Start training the model.
     history = model.fit(
         train_gen,
         validation_data=valid_gen,
@@ -161,52 +136,36 @@ with mlflow.start_run(run_name=f'{model_type}_model'):
 
     print(f'[INFO] Successfully Completed {model_type} Model Training')
 
-    # Training End
     te_time = time.time()
     t2 = (te_time-s_time)/60
     print(f'\33[5;30;46m [INFO] Model Training Completed in {round(t2, 2)} Minutes \33[0m')
 
-    # Evaluate the trained model.
     model_evaluation_history = model.evaluate(valid_gen)
-
-    # Get the loss and accuracy from model_evaluation_history.
     model_evaluation_loss, model_evaluation_accuracy = model_evaluation_history
-
-    # Define a useful name for our model to make it easy for us while navigating through multiple saved models.
     model_file_name = f'{model_type}_model_loss_{model_evaluation_loss:.3}_acc_{model_evaluation_accuracy:.3}.h5'
 
-    # Save your Model
     path_to_save_model = os.path.join(path_to_model_dir, model_file_name)
     model.save(path_to_save_model)
     print(f'[INFO] Model {model_file_name} saved Successfully..')
 
-    # Model Size
     mb_size = os.path.getsize(f'{path_to_save_model}')
     mb_size = round(mb_size / 1e+6, 2)
     print(f'[INFO] {model_type} Model Size: {mb_size} MB')
 
-    # Plot History
     metric_loss = history.history['loss']
     metric_val_loss = history.history['val_loss']
     metric_accuracy = history.history['accuracy']
     metric_val_accuracy = history.history['val_accuracy']
 
-    # Construct a range object which will be used as x-axis (horizontal plane) of the graph.
     epochs = range(len(metric_loss))
 
-    # Plot the Graph.
     plt.plot(epochs, metric_loss, 'blue', label=metric_loss)
     plt.plot(epochs, metric_val_loss, 'red', label=metric_val_loss)
     plt.plot(epochs, metric_accuracy, 'magenta', label=metric_accuracy)
     plt.plot(epochs, metric_val_accuracy, 'green', label=metric_val_accuracy)
-
-    # Add title to the plot.
     plt.title(str('Model Metrics'))
-
-    # Add legend to the plot.
     plt.legend(['loss', 'val_loss', 'accuracy', 'val_accuracy'])
 
-    # If the plot already exist, remove
     metrics_png_name = f'{model_type}_metrics.png'
     path_to_metrics = os.path.join(path_to_model_dir, metrics_png_name)
     plot_png = os.path.exists(path_to_metrics)
@@ -217,7 +176,6 @@ with mlflow.start_run(run_name=f'{model_type}_model'):
         plt.savefig(path_to_metrics, bbox_inches='tight')
     print(f'[INFO] Successfully Saved {metrics_png_name}')
 
-    # MLFlow Metrics
     mlflow.log_metric('Input Image Size', IMAGE_SIZE)
     mlflow.log_metric('Total Image Data', total_data)
     mlflow.log_metric('Train Size', train_size)
